cb259615dc5b189bc3190db435f857bf7f8f6bd7
Sara Ogaz <ogaz@stsci.edu>
2018-09-16 13:54:58 -0400

    adding ctestis code and tests

--- stistools/__init__.py
+++ stistools/__init__.py
@@ -13,6 +13,7 @@ from . import stisnoise
 from . import wx2d
 from . import doppinfo
 from . import tastis
+from . import ctestis
 
 # These lines allow TEAL to print out the names of TEAL-enabled tasks
 # upon importing this package.
